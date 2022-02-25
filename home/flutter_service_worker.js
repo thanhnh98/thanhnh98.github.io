1641b643eff557f056c746b76d830cecaa976411
Thanh <thanh150898@gmail.com>
2022-02-25 17:17:14 +0700

    update

--- home/flutter_service_worker.js
+++ home/flutter_service_worker.js
@@ -16,8 +16,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "30e3cbce4863e95e97ce4b9323500c8c",
-"/": "30e3cbce4863e95e97ce4b9323500c8c",
+"index.html": "9a9e117ee74263767781ec94e72f1625",
+"/": "9a9e117ee74263767781ec94e72f1625",
 "main.dart.js": "a7cecbc56b343da7c2e16b93b41c8f81",
 "manifest.json": "3a22f4a8eda9cba7b59533b6e0cc4dcc",
 "version.json": "fc4a4cbbb9b61af76710aa6de5c13fdb"
