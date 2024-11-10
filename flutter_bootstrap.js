18ffa536e2c05b0d56e143d6b3891430c614a19e
Thanhnh <thanh.nguyenhoai@cbtw.tech>
2024-11-11 00:14:44 +0700

    release update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "738910014"
+    serviceWorkerVersion: "52156101"
   }
 });
