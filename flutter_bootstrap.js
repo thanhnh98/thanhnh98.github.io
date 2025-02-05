e7a647d3196989b3b3ff154c80c546510dd1c735
Thanhnh <thanh.nguyenhoai@cbtw.tech>
2025-02-05 14:12:58 +0700

    new year

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3072508173"
+    serviceWorkerVersion: "2081069623"
   }
 });
