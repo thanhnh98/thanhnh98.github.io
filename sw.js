721b99472ec92762ba2a3e2f1088e3186d9117af
GitHub Action <action@github.com>
2026-02-04 03:46:55 +0000

    chore: auto-update version for deployment [skip ci]

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker for Sắp Tết PWA
 // Version được tự động update bởi scripts/update-version.js khi deploy
-const CACHE_NAME = 'sap-tet-v1.0.1770175250454-a0c03ed';
+const CACHE_NAME = 'sap-tet-v1.0.1770176815566-57f3975';
 
 // Invalid subdomains list
 const INVALID_SUBDOMAINS = [
