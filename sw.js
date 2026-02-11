8f6a0989486f36da12cfd026982fe64fd94a9c86
thanhnh98 <thanhnh98@users.noreply.github.com>
2026-02-11 02:02:49 +0000

    deploy: 9d4fed1c1962f3e95b92c4a8aebf4fa14934d329

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker for Sắp Tết PWA
 // Version được tự động update bởi scripts/update-version.js khi deploy
-const CACHE_NAME = 'sap-tet-v1.0.1770775355820-e08e0f4';
+const CACHE_NAME = 'sap-tet-v1.0.1770775367946-9d4fed1';
 
 // Invalid subdomains list
 const INVALID_SUBDOMAINS = [
