c56357e7506b49693a2f4e9be1ea6c17461a0a0d
Thanhnh <thanh.nguyenhoai@cbtw.tech>
2026-04-10 09:01:23 +0700

    chore: auto-update version for deployment [skip ci]

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker for Sắp Tết PWA
 // Version được tự động update bởi scripts/update-version.js khi deploy
-const CACHE_NAME = 'sap-tet-v1.0.1775724629783-2bbbcdf';
+const CACHE_NAME = 'sap-tet-v1.0.1775786483056-5aa12ba';
 
 // Invalid subdomains list
 const INVALID_SUBDOMAINS = [
